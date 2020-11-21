e08737242fbf13f98d622ae32fb99b2d61ab93f3
AlejandraSC <alejandrasalmeron6@gmail.com>
2020-11-21 14:51:00 +0100

    Include the state on App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,18 @@ import pokemons from '../data/pokemons.json';
 import PokeList from './PokeList';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons: pokemons,
+    };
+  }
+
   render() {
     return (
       <div>
         <h1 className="mainTitle">Mi lista de pokemons</h1>
-        <PokeList pokemonsList={pokemons} />
+        <PokeList pokemonsList={this.state.pokemons} />
       </div>
     );
   }
